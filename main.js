c707acea2080f7a2be09d9bf44c9b4ae534678c4
Grigoriy Efimov <efimov-gm@newpage.xyz>
2023-05-06 19:14:58 +0300

    Improve appearance

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 window.onload = () => {
     window.animationInterval = 0;
     window.step =1;
+    window.animationType = 0;
+
+    change_phase1_val();
+    change_phase2_val();
+    change_freq1_val();
+    change_freq2_val();
+    change_amp1_val();
+    change_amp2_val();
     redraw();
 
 }
@@ -17,7 +25,7 @@ redraw = () => {
     let A1 = amp1.value/2;
     let A2 = amp2.value/2;
 
-    draw_lissajous(omega1, omega2, phi1, phi2, A1, A2, cnv, 0.1)
+    draw_lissajous(omega1, omega2, phi1, phi2, A1, A2, cnv, 10)
 }
 
 draw_lissajous = (omega1, omega2, phi1, phi2, A1coef, A2coef, canvas, step) => {
@@ -28,7 +36,7 @@ draw_lissajous = (omega1, omega2, phi1, phi2, A1coef, A2coef, canvas, step) => {
     
     ctx.clearRect(0,0,canvas.height,canvas.width)
 
-    for(t=0; t < Math.PI/(omega1*omega2); t+=step ){
+    for(t=0; t < 100000; t+=step ){
         let x =  150 + A1 * Math.cos( omega1 * t + phi1)
         let y = 150 - A2 * Math.cos( omega2 * t+ phi2)
         ctx.fillRect(x,y,1,1);
@@ -39,7 +47,7 @@ start_animation= () => {
     window.step =0;
     button_start.classList.add("visually-hidden");
     button_stop.classList.remove("visually-hidden");
-    window.animationInterval = setInterval( () => { draw_animation() }, 40)
+    window.animationInterval = setInterval( () => { draw_animation() }, 60)
 }
 
 stop_animation= () => {
@@ -50,18 +58,51 @@ stop_animation= () => {
 }
 
 draw_animation = () => {
-    let coeff= window.step % 100;
+    let coeff= window.step % 2000;
     window.step++;
 
-    let freq1 = 0.001;
-    let freq2 = 0.8 * freq1;
-    
-    let phi1 = coeff/50* Math.PI;
-    let phi2 = Math.PI*0.1;
-    
-    let A1 = 75;
-    let A2 = 75;
+    let freqval1 = freq1.value/10;
+    let freqval2 = freq2.value/10;
 
-    draw_lissajous(freq1, freq2, phi1, phi2, A1, A2, cnv, 2000)
+    let phi1 = phase1.value * Math.PI * 2;
+    let phi2 = phase2.value * Math.PI * 2;
+    
+    if(!window.animationType){
+        freqval2 = 0.8 * freqval1;    
+        phi1 = coeff/50* Math.PI;
+    }
+    else{
+        freqval1 = 0.001;
+        freqval2 = freqval1 * coeff/1000;
+    }
 
+    let A1 = amp1.value/2;
+    let A2 = amp2.value/2;
+
+    draw_lissajous(freqval1, freqval2, phi1, phi2, A1, A2, cnv, 5)
+
+}
+
+change_phase1_val = () => {
+    phase1_val.innerHTML= ((phase1.value * Math.PI * 2) + "").substring(0,5)
+}
+change_phase2_val = () => {
+    phase2_val.innerHTML= ((phase2.value * Math.PI * 2) + "").substring(0,5)
+}
+change_freq1_val = () => {
+    freq1_val.innerHTML= ((freq1.value) + "").substring(0,5)
+    draw_freq_diff();
+}
+change_freq2_val = () => {
+    freq2_val.innerHTML= ((freq2.value) + "").substring(0,5)
+    draw_freq_diff();
+}
+change_amp1_val = () => {
+    amp1_val.innerHTML= ((amp1.value) + "").substring(0,5)
+}
+change_amp2_val = () => {
+    amp2_val.innerHTML= ((amp2.value) + "").substring(0,5)
+}
+draw_freq_diff = () => {
+    freq_diff_val.innerHTML= ((freq1.value/freq2.value) + "").substring(0,5)
 }
